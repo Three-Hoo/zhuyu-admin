1fe73e67e24aac8d7632cb3218e68cb168769e70
鹤 <fuhe@sanhe.org.cn>
2023-10-21 17:13:48 +0800

    feat: 运行

--- upload-ssh.sh
+++ upload-ssh.sh
@@ -14,21 +14,20 @@ rm -rf .next
 
 # 安装依赖...
 echo "开始安装依赖..."
-npm install > /dev/null
+pnpm install
 
-# # 打包
-# echo "开始打包..."
-# npm run build > /dev/null
+echo "检查 build 是否报错..."
+pnpm build
 
-rm -rf zhuyu-admin-bunder.zip
-zip -r zhuyu-admin-bunder.zip package.json package-lock.json next.config.js .env public tsconfig.json postcss.config.js global.d.ts
+rm -rf zhuyu-admin-bunder.zip build .next
+zip -r zhuyu-admin-bunder.zip src prisma package.json package-lock.json next.config.js .env public tsconfig.json postcss.config.js global.d.ts
 echo "文件大小：$(du -h zhuyu-admin-bunder.zip)"
 
 echo "删除上上一个备份..."
 ssh -p $PORT root@$HOST rm -rf $APP_DIR/zhuyu-admin-bunder-bak.zip 
 
 # echo "备份上次打包文件..."
-# ssh -p $PORT root@$HOST zip -r $APP_DIR/zhuyu-admin-bunder-bak.zip $APP_DIR/zhuyu-admin/package.json $APP_DIR/zhuyu-admin/package-lock.json $APP_DIR/zhuyu-admin/next.config.js $APP_DIR/zhuyu-admin/.env $APP_DIR/zhuyu-admin/.next $APP_DIR/zhuyu-admin/public
+# ssh -p $PORT root@$HOST zip -r $APP_DIR/zhuyu-admin-bunder-bak.zip $APP_DIR/package.json $APP_DIR/src $APP_DIR/package-lock.json $APP_DIR/next.config.js $APP_DIR/.env $APP_DIR/public $APP_DIR/tsconfig.json $APP_DIR/postcss.config.js $APP_DIR/global.d.ts
 
 echo "上传文件..."
 scp -P $PORT zhuyu-admin-bunder.zip root@$HOST:$APP_DIR/
@@ -40,10 +39,13 @@ echo "解压文件..."
 ssh -p $PORT root@$HOST unzip $APP_DIR/zhuyu-admin-bunder.zip -d $APP_DIR/zhuyu-admin/
 
 echo "服务器上安装依赖..."
-ssh -p $PORT root@$HOST "cd $APP_DIR/zhuyu-admin && pwd && npm ci"
+ssh -p $PORT root@$HOST "cd $APP_DIR/zhuyu-admin && pwd && pnpm install"
 
 echo "开始打包..."
-ssh -p $PORT root@$HOST "cd $APP_DIR/zhuyu-admin && npm run build"
+ssh -p $PORT root@$HOST "cd $APP_DIR/zhuyu-admin && pnpm generate && pnpm build"
 
 echo "重启服务..."
 ssh -p $PORT root@$HOST "cd $APP_DIR/zhuyu-admin && pm2 start npm -- start"
+
+echo "成功，清除打包文件"
+rm -rf zhuyu-admin-bunder.zip build .next
