fbf47f6cd45c1e0ec5ea297f78c17e1a7e6df3d0
鹤 <fuhe@sanhe.org.cn>
2023-11-13 16:48:29 +0800

    =

--- src/sources/value-enum/prompt-template-type.ts
+++ src/sources/value-enum/prompt-template-type.ts
@@ -1,7 +1,15 @@
 export const PROMPT_TEMPLATE_TYPE = {
   CHAT: '聊天',
-  SYNTAX: '语法分析',
+  //
+  SYNTAX: '句型分析',
+  //
+  CHINA_SPEECH: '中文差异',
+  //
   SYNTAX_RECOLOR: '语法润色',
+  //
+  SYNTAX_CHECK: '语法检查',
+  //
   REPLY: '教我回复',
+  //
   OTHER: '其他',
 }
